a46879cc0a4ca52bde557f1ef2734cfc39ef9799
vonghoanh <hoanhvong@gmail.com>
2020-09-22 16:16:25 +0700

    [hotfix] change kind

--- crawler.py
+++ crawler.py
@@ -50,6 +50,7 @@ def get_all_product_by_url (headers: dict, url: str, ward: str ,limitPage: int =
     zone = []
     date = []
     description = []
+
     response_base = get(url, headers=headers)
     html_soup_base = BeautifulSoup(response_base.text, 'html.parser')
     product_container = html_soup_base.find_all("div", "product-main")
@@ -60,100 +61,112 @@ def get_all_product_by_url (headers: dict, url: str, ward: str ,limitPage: int =
 
         #Parser data from a page
         for i in range(0, len_prod-1):
-            try:
-                tags_nha = "ban-nha"
-                tags_can_ho = "ban-can-ho-chung-cu"
-                house_container = product_container[i]
+            tags_nha = "ban-nha"
+            tags_can_ho = "ban-can-ho-chung-cu"
+            house_container = product_container[i]
 
-                #title pose 
-                title_value = house_container.a.get('title')
-                if (len(title_value) > 0 ): 
-                    title.append(title_value)
+            title_value = ""
+            kind_house_value = "unknown"
+            link_detail_value = ""
+            area_value = 0
+            price_value = 0
+            date_value = ""
+            zone_value = ""
+            description_value = ""
+
+            #title pose 
+            title_value_ = house_container.a.get('title')
+            if (len(title_value) > 0 ): 
+                title_value = title_value_
+    
+            #ward
+            ward_list.append(ward)
+
+            #title nha_ban/chung_cu
+            kind_house = house_container.find_all('a', 'vipZero product-link')
+            if (str(kind_house).find(tags_nha) > 0):
+                kind_house_value = tags_nha
+            elif(str(kind_house).find(tags_can_ho) > 0):
+                kind_house_value = tags_can_ho
+
+            #link detail
+            link_detail_ = house_container.find_all('h3', 'product-title')[0].a.get('href')
+            link_detail_value = link_detail_
+
+
+            #area 
+            area_value_ = house_container.find_all('span', 'area')
+            if (len(area_value_) > 0):
+                normal_area = area_value_[0].text.replace("m²", "").replace(" ", "")
+                if(len(re.findall(r"[a-z]", normal_area)) == 0):
+                    area_value = float(normal_area)
                 else:
-                    title.append("")
+                    area_value = 0
+            else:
+                area_value = 0
 
-                #ward
-                ward_list.append(ward)
+            #price
+            price_value_ = house_container.find_all('span', 'price')
+            if (len(price_value_) > 0):
 
-                #title nha_ban/chung_cu
-                kind_house = house_container.find_all('a', 'vipZero product-link')
-                if (str(kind_house).find(tags_nha) > 0):
-                    kind.append(tags_nha)
-                elif(str(kind_house).find(tags_can_ho) > 0):
-                    kind.append(tags_can_ho)
-                else: 
-                    kind.append("unknown")
+                #gia thoa thuan
+                if (price_value_[0].text.replace(" ", "").isalpha()):
+                    price_value = -1
 
-                #link detail
-                link_detail_value = house_container.find_all('h3', 'product-title')[0].a.get('href')
-                link_detail.append(link_detail_value)
-
-
-                #area 
-                area_value = house_container.find_all('span', 'area')
-                if (len(area_value) > 0):
-                    normal_area = area_value[0].text.replace("m²", "").replace(" ", "")
-                    if(len(re.findall(r"[a-z]", normal_area)) == 0):
-                        area.append(float(normal_area))
-                    else:
-                        area.append(0)
+                #gia tr/m2
+                elif(len(re.findall(r"/m²", price_value_[0].text)) > 0): 
+                    rate = float(price_value_[0].text.split(" ")[0])
+                    if (len(re.findall(r"triệu", price_value_[0].text)) > 0):
+                        price_value = rate * area_value * 1000000
+                    if (len(re.findall(r"tỷ", price_value_[0].text)) > 0):
+                        price_value = rate * area_value * 1000000000
+                #gia binh thuong
                 else:
-                    area.append(0)
-
-                #price
-                price_value = house_container.find_all('span', 'price')
-                if (len(price_value) > 0):
-
-                    #gia thoa thuan
-                    if (price_value[0].text.replace(" ", "").isalpha()):
-                        price.append(-1)
-
-                    #gia tr/m2
-                    elif(len(re.findall(r"/m²", price_value[0].text)) > 0): 
-                        rate = float(price_value[0].text.split(" ")[0])
-                        if (len(re.findall(r"triệu", price_value[0].text)) > 0):
-                            price.append(rate * area[i] * 1000000)
-                        if (len(re.findall(r"tỷ", price_value[0].text)) > 0):
-                            price.append(rate * area[i] * 1000000000)
-                    #gia binh thuong
-                    else:
-                        rate = float(price_value[0].text.split(" ")[0])
-                        if (len(re.findall(r"triệu", price_value[0].text)) > 0):
-                            price.append(rate * 1000000)
-                        if (len(re.findall(r"tỷ", price_value[0].text)) > 0):
-                            price.append(rate * 1000000000)
-                else:   
-                    price.append(0)
+                    rate = float(price_value_[0].text.split(" ")[0])
+                    if (len(re.findall(r"triệu", price_value_[0].text)) > 0):
+                        price_value = rate * 1000000
+                    if (len(re.findall(r"tỷ", price_value_[0].text)) > 0):
+                        price_value = rate * 1000000000
+            else:   
+                price_value = 0
 
 
-                #zone
-                zone_value = house_container.find_all('span', 'location')
-                if (len(zone_value) >0):
-                    zone.append(zone_value[0].text)
-                else:
-                    zone.append("")
+            #zone
+            zone_value_ = house_container.find_all('span', 'location')
+            if (len(zone_value_) >0):
+                zone_value = zone_value_[0].text
+            else:
+                zone_value = ""
 
-                #date
-                date_value = house_container.find_all('span', 'tooltip-time')
-                if (len(date_value) > 0): 
-                    date.append(date_value[0].text)
-                else:
-                    date.append("")
-                
+            #date
+            date_value_ = house_container.find_all('span', 'tooltip-time')
+            if (len(date_value_) > 0): 
+                date_value = date_value_[0].text
+            else:
+                date_value = ""
+            
 
-                #description
-                description_value = house_container.find_all('div', 'product-content')
-                if (len(description_value) > 0 ):
-                    description.append(description_value[0].text)
-                else: 
-                    description.append("")
+            #description
+            description_value_ = house_container.find_all('div', 'product-content')
+            if (len(description_value_) > 0 ):
+                description_value = description_value_[0].text
+            else: 
+                description_value = ""
 
-                
-                # sleep
-                time.sleep(random.randint(1,2))
+            
+            # add to pd
+            title.append(title_value)
+            ward_list.append(ward)
+            link_detail.append(link_detail_value)
+            kind.append(kind_house_value)
+            price.append(price_value)
+            area.append(area_value)
+            zone.append(zone_value)
+            date.append(date_value)
+            description.append(description_value)
 
-            except Exception:
-                print("fail to crawl data at ward {0} page {1} error {2}".format(ward, page, Exception))
+        # sleep
+            time.sleep(random.randint(1,2))
 
         # Get next page
         page = page+1
@@ -166,7 +179,7 @@ def get_all_product_by_url (headers: dict, url: str, ward: str ,limitPage: int =
 
     return pd.DataFrame({
         'Title': title,
-        'Ward' : ward,
+        'Ward' : ward_list,
         'LinkDetail' : link_detail,
         'Kind': kind,
         'Price': price,
@@ -177,15 +190,14 @@ def get_all_product_by_url (headers: dict, url: str, ward: str ,limitPage: int =
     })
 
 
-def crawler_start(i:int):
+def crawler_start(i:int, district_url:str):
     
     headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
     root_page_url = "https://batdongsan.com.vn"
-    list_district_url = get_list_district_url(headers)
 
     # get district page
     list_estate = pd.DataFrame()
-    url = root_page_url + list_district_url[i]
+    url = root_page_url + district_url
     list_ward_url = get_ward_navigate_url(headers, url)
 
     # crawler estate at ward page
@@ -205,19 +217,22 @@ def crawler_start(i:int):
     print("finish")
 
 def crawler_start_(i):
-    return crawler_start(i)
+    headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+    list_district_url = get_list_district_url(headers)
+
+    return crawler_start(i, list_district_url[i])
 
 
 def crawl_multi_thread (n_process: int):
     list_estate = pd.DataFrame()
 
     headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
-    root_page_url = "https://batdongsan.com.vn"
     list_district_url = get_list_district_url(headers)
+
     print("Number of district {0}".format(len(list_district_url)))
 
     if (n_process == 1):
-        list_estate = crawler_start()
+        list_estate = crawler_start(0)
     else:
         with Pool(n_process) as pool:
             for v in pool.imap(crawler_start_, range(0, len(list_district_url)-1)):
